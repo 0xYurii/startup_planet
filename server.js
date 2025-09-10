57142dd9981fb8b8051ede26255249776b208915
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-10 11:41:22 +0100

    serve static data with query filtering

--- server.js
+++ server.js
@@ -1,19 +1,47 @@
 import express from "express"
-
+import { startups } from "./data/data.js"
 
 const PROT=3000
 
-const celebrity = {
-  type: "action hero",
-  name: "JSON Statham"
-}
+
+let filteredStartups=startups
 
 const app=express()
 
 app.get('/api',(req,res)=>{
     if(req.method==="GET"){
-        res.json(celebrity)
-    }else{}
+        const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
+        console.log(req.query)
+
+        if(industry){
+            filteredStartups=filteredStartups.filter(startup=>
+                startup.industry.toLowerCase()===industry.toLowerCase()
+            )
+        }
+        if(country){
+            filteredStartups=filteredStartups.filter(startup=>
+                startup.country.toLowerCase()===country.toLowerCase()
+            )
+        }
+        if(continent){
+            filteredStartups=filteredStartups.filter(startup=>
+                startup.continent.toLowerCase()===continent.toLowerCase()
+            )
+        }
+        if(is_seeking_funding){
+            filteredStartups=filteredStartups.filter(startup=>
+                startup.is_seeking_funding===JSON.parse (is_seeking_funding.toLowerCase())
+            )
+        }
+        if(has_mvp){
+            filteredStartups=filteredStartups.filter(startup=>
+                startup.has_mvp===JSON.parse (has_mvp.toLowerCase())
+            )
+        }
+
+
+        res.json(filteredStartups)
+    }
     
 })
 
