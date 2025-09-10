6667059c8632f23a62488564da8050a3519c0b93
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-10 16:16:19 +0100

    add api/<field>/<term> end point and serve data based on req url

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ import { startups } from "./data/data.js"
 const PROT=3000
 
 
-let filteredStartups=startups
+
 
 const app=express()
 
 app.get('/api',(req,res)=>{
-    if(req.method==="GET"){
+    let filteredStartups=startups
         const { industry, country, continent, is_seeking_funding, has_mvp } = req.query
         console.log(req.query)
 
@@ -41,11 +41,52 @@ app.get('/api',(req,res)=>{
 
 
         res.json(filteredStartups)
-    }
+    
     
 })
 
 
+app.get("/api/:field/:term",(req,res)=>{
+    let filteredStartups=startups
+        //devide the rea.params
+        const {field,term}=req.params
+        console.log(field)
+        console.log(term)
+
+        // server data based on reqested link
+        filteredStartups=filteredStartups.filter(startups=>
+            startups[field].toLowerCase()===term.toLowerCase()
+        )
+        
+        res.json(filteredStartups)
+    
+})
+
+
+/*
+Challenge:
+1. Add a new route which accepts GET requests to /api/<field>/<term>.
+2. Filter the data based on the path params.
+3. Serve the filtered data.
+
+For now, don’t worry that using some fields will trigger an error.
+
+** The functionality **
+Get all startups in a given country via api/country/<country name>
+Get all startups in a given continent via api/continent/<continent name>
+Get all startups in a given industry via api/industry/<industry name>
+
+**Test Cases** 
+
+These should work:
+  api/country/india
+  api/continent/europe
+  api/industry/ai
+
+This will throw an error - but that's fine!
+	api/has_mvp/true
+
+*/
 
 
 app.listen(PROT,()=>{
