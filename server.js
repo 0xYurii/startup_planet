f14264989ed2968cd6b12e4620096545f9e0c7be
Younes Hebaiche <126246603+0xYurii@users.noreply.github.com>
2025-09-10 10:49:49 +0100

    add some data and serve at at /api end point

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("hi")
+import express from "express"
+
+
+const PROT=3000
+
+const celebrity = {
+  type: "action hero",
+  name: "JSON Statham"
+}
+
+const app=express()
+
+app.get('/api',(req,res)=>{
+    if(req.method==="GET"){
+        res.json(celebrity)
+    }else{}
+    
+})
+
+
+
+
+app.listen(PROT,()=>{
+    console.log(`The server is runnin on http://localhost:${PROT}`)
+    console.log('Go check it out in your browser! 🌟');
+})
